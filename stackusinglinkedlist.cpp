2483e0344aa21ab6b4ac0152d991fbff1fb0a842
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-10 17:09:24 +0700

    membuat fungsi push dengan tipe data int di dalam class Stack

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -14,3 +14,11 @@ public:
 class Stack {
     private:
     Node* top; // Pointer to the top node of the stack
+
+public:
+    Stack() {
+        top = NULL; // Initialize the stack with a null top pointer
+
+    } 
+
+     int push(int value) {
